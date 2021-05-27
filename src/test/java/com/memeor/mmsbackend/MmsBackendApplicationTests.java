a3bc107b5c4d66c0f7c74fd8ecfe0b383dbba464
Bivash Yadav <31441862+byadav02717@users.noreply.github.com>
2021-05-27 17:47:37 -0500

    Initialized spring-boot project.

---
+++ src/test/java/com/memeor/mmsbackend/MmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memeor.mmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
