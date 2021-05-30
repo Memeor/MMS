c46355bf5c29af9e366ee0718c8eac8d38d397b7
Rabindra Yadav <rabindrayadav9596@gmail.com>
2021-05-30 17:28:49 -0500

    updated app.js

--- mms-frontend/src/App.js
+++ mms-frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>MMS</p>
+        <p>Members Management Software</p>
        
       </header>
     </div>
