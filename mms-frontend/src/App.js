0ec3b434d1e3db7bc6fc4646cf3c5c12f50b560c
Bivash Yadav <byadav02717@gmail.com>
2021-06-13 10:52:24 -0500

    Added pages css folder, created routes

--- mms-frontend/src/App.js
+++ mms-frontend/src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import AppInstall from './pages/AppInstall';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Members Management Software</p>
-       
-      </header>
+    <Router>
+    <div>
+
     </div>
+
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/install" component = {AppInstall} />
+    
+    </Switch>
+    </Router>
   );
 }
 
---
+++ mms-frontend/src/pages/AppInstall.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class AppInstall extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h3>This is the page for installing app for an organizaation for the first time.</h3>
+            </div>
+        )
+    }
+}
+
+export default AppInstall
