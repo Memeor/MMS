f505c7fb1088dd09dfbbeb772d8c882eb1a9d7fa
Bivash Yadav <byadav02717@gmail.com>
2021-06-15 23:04:34 -0500

    added input validation

--- mms-frontend/src/pages/AppInstall.js
+++ mms-frontend/src/pages/AppInstall.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Grid,Paper,Typography,Button, TextField } from '@material-ui/core'
+import validator from 'validator';
 
 export class AppInstall extends Component {
     constructor(props) {
@@ -10,32 +11,96 @@ export class AppInstall extends Component {
             address:"",
             email:"",
             phone:"",
-            description:""
+            description:"",
+            password:"",
+            confirmPassword:"",
+            valid:false,
+            error:{
+                emailError:false,
+                eamilErrorDesc:"Email*",
+                passwordError:false,
+                passwordErrorDesc:"",
+                confirmPasswordError:false,
+                confirmPasswordErrorDesc:""
+            }
+            
         };
-        //this.submitfunction = this.submitfunction.bind;
+        
 
     }
     
-    // state={
-    //     name:"",
-    //     address:"",
-    //     email:"",
-    //     phone:"",
-    //     description:""
 
-
-    // };
 
     change = e =>{
+        let errors = this.state.error;
         this.setState({
             [e.target.name]: e.target.value
         });
-        //console.log(this)
-    }
+        if(e.target.name === "email")
+        {
+            if(validator.isEmail(this.state.email))
+            {
+                errors["eamilErrorDesc"]="Email*";
+                errors["emailError"]=false;
+                this.setState({error:errors})
+            }
+            else
+            {
+                errors["eamilErrorDesc"]="Email*: Please type a valid Email address";
+                errors["emailError"] = true;
+                this.setState({error:errors})
 
+            }
+
+        }
+        else if(e.target.name === "password")
+        {
+            
+            if(validator.isLength(e.target.value, 8, 15) && validator.isStrongPassword(e.target.value))
+            {
+                errors["passwordErrorDesc"]="";
+                errors["passwordError"]=false;
+                this.setState({error:errors})
+
+            }
+            else
+            {
+                errors["passwordErrorDesc"]="Password should be the combination of atleast 1 lowercase letter, 1 uppercase letter, 1 number and 1 symbol and has length of atleast 8-15 characters.";
+                errors["passwordError"] = true;
+                this.setState({error:errors})
+
+            }
+        }
+        else if(e.target.name === "confirmPassword")
+        {
+            if(validator.equals(e.target.value, this.state.password))
+            {
+                errors["confirmPasswordErrorDesc"]="";
+                errors["confirmPasswordError"]=false;
+                this.setState({error:errors})
+
+            }
+            else
+            {
+                errors["confirmPasswordErrorDesc"]="Password didn't match";
+                errors["confirmPasswordError"] = true;
+                this.setState({error:errors})
+
+            }
+        }
+        this.setState({
+            valid: !(this.state.error.emailError || this.state.error.passwordError || this.state.error.confirmPasswordError || !validator.isLength(this.state.name1, 1,50) || !validator.isLength(this.state.address, 1, 200) || ! validator.isLength(this.state.description, 1, 10000))
+        });
+
+        
+       
+    }
+ 
+    
     submitfunction=()=>
     {
-        console.log(this)
+        //var valid = this.handleValidation();
+        console.log(this.state)
     }
     
     
@@ -45,7 +110,7 @@ export class AppInstall extends Component {
         return (
             <div>
                 <Grid>
-                    <Paper elevation="20" style={paperStyle}>
+                    <Paper elevation={10} style={paperStyle}>
                         <h2>Organization Setup</h2>
                     <Typography variant='caption'>Fill up the information to setup an organization</Typography>
 
@@ -54,10 +119,10 @@ export class AppInstall extends Component {
                     <div>
                         
                         <TextField
-                        id="outlined-basic"
+                        //id="outlined-basic"
                         type ="text"
                         fullWidth
-                        label="Organization Name"
+                        label="Organization Name*"
                         name = "name1"
                         value={this.state.name}
                         
@@ -69,9 +134,9 @@ export class AppInstall extends Component {
                     <div>
                        
                     <TextField
-                        label="Address"
+                        label="Address*"
                         name="address"
-                        id="standard-basic"
+                        //id="standard-basic"
                         type ="text"
                         fullWidth
                         placeholder="Physical office Adderess"
@@ -81,9 +146,10 @@ export class AppInstall extends Component {
                      <div>
                         
                      <TextField
-                        label="Email"
+                        error={this.state.error.emailError}
+                        label={this.state.error.eamilErrorDesc}
                         name="email"
-                        id="standard-basic"
+                        //id="standard-basic"
                         type ="email"
                         fullWidth
                         placeholder="Email of the Organization"
@@ -96,9 +162,9 @@ export class AppInstall extends Component {
                     <div>
                         
                     <TextField
-                        label="Phone number"
+                        label="Phone number*"
                         name="phone"
-                        id="standard-basic"
+                        //id="standard-basic"
                         type ="tel"
                         placeholder=""
                         onChange={e => this.change(e)}
@@ -109,9 +175,47 @@ export class AppInstall extends Component {
                     <div>
                         
                     <TextField
-                        label="Description"
+                        label="Password*"
+                        name="password"
+                        //id="standard-basic"
+                        type ="password"
+                        error={this.state.error.passwordError}
+                        placeholder="Enter a strong password"
+                        onChange={e => this.change(e)}
+
+                        />
+                       
+                        </div>
+                        
+                    <div>
+                        <Typography variant='caption'>{this.state.error.passwordErrorDesc}</Typography>
+                      
+                    </div>
+                    
+
+                    <div>
+                    <TextField
+                        label="Confirm Password*"
+                        name="confirmPassword"
+                        //id="standard-basic"
+                        type ="password"
+                        error={this.state.error.confirmPasswordError}
+                        placeholder="Enter a strong password"
+                        onChange={e => this.change(e)}
+
+                        />
+                      
+                    </div>
+                    <div>
+                        <Typography variant='caption'>{this.state.error.confirmPasswordErrorDesc}</Typography>
+                      
+                    </div>
+                    <div>
+                        
+                    <TextField
+                        label="Description*"
                         name="description"
-                        id="standard-basic"
+                        //id="standard-basic"
                         type ="text"
                         fullWidth
                         multiline
@@ -134,7 +238,7 @@ export class AppInstall extends Component {
                     <br />
 
                     <div>
-                        <Button variant='contained' color='primary' onClick={this.submitfunction}>Submit</Button>
+                        <Button disabled={! this.state.valid} variant='contained' color='primary' onClick={this.submitfunction}>Submit</Button>
                     </div>
                       
                     </div>
