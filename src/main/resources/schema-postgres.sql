53a40cd638917e52fb3f11106be3349bba2d3133
Bivash Yadav <byadav02717@gmail.com>
2021-07-21 15:16:27 -0500

    Added database table

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -24,3 +24,15 @@ INSERT INTO mms_user(username, password, email_address) VALUES('losangeles', '39
 INSERT INTO mms_user(username, password, email_address) VALUES('newyork', '855000xyz0', 'ny@europe.eu');
 INSERT INTO mms_user(username, password, email_address) VALUES('edinburgh', '464000xyz', 'edinburgh@europe.eu');
 INSERT INTO mms_user(username, password, email_address) VALUES('berlin', '671000xyz', 'berlin@europe.eu');
+
+DROP TABLE IF EXISTS organizations;
+CREATE TABLE organizations (
+	id serial PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	address VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	ph_number VARCHAR(255) NOT NULL,
+	description VARCHAR(1000) NOT NULL,
+	created_ts TIMESTAMP WITH TIME ZONE,
+	updated_ts TIMESTAMP WITH TIME ZONE
+);
